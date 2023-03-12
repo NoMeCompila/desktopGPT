e75ab2486f9120a1cac10da8a662738794e54be6
Fernando Caballero <cabafer1@gmail.com>
2023-03-12 06:43:46 +0100

    Conexion con openAI

--- desktopGPT.py
+++ desktopGPT.py
@@ -1,5 +1,27 @@
 from tkinter import *
+import openai
 
+
+def imprimir_hola_mundo():
+    output_textbox.insert("end", "Hola Mundo\n")
+
+
+def borrar_texto():
+    output_textbox.delete(1.0, "end")
+
+
+def chat_gpt():
+    borrar_texto()
+    openai.api_key = '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'
+
+    my_prompt = input_textbox.get("1.0", END)
+    if my_prompt == '':
+        output = 'ingrese texto válido'
+
+    completion = openai.Completion.create(engine='text-davinci-003', prompt=my_prompt, max_tokens=4000, temperature=0.3)
+    output = completion.choices[0].text
+
+    output_textbox.insert("end", output)
 
 
 if __name__ == '__main__':
@@ -20,9 +42,7 @@ if __name__ == '__main__':
     windows.configure(background="#444654")
 
     # chat text label config
-    input_label = Label(windows,
-                        text="PROMPT",
-                        bg="#444654")
+    input_label = Label(windows, text="PROMPT", bg="#444654")
     input_label.config(fg="white", font=("Roboto", 12))
     input_label.pack(pady=5)
 
@@ -40,9 +60,7 @@ if __name__ == '__main__':
             )
     input_textbox.pack(pady=10)
 
-    output_label = Label(windows,
-                        text="OUTPUT",
-                        bg="#444654")
+    output_label = Label(windows, text="OUTPUT", bg="#444654")
     output_label.config(fg="white", font=("Roboto", 12))
     output_label.pack(pady=5)
 
@@ -60,12 +78,11 @@ if __name__ == '__main__':
             )
     output_textbox.pack(pady=5)
 
-
-    def imprimir_hola_mundo():
-        output_textbox.insert("end", "Hola Mundo\n")
     # Make a custom button
-    button = Button(windows, text="Responder", bg="gray15", fg="white", relief="flat", cursor="hand2", bd=0, padx=10, command=imprimir_hola_mundo)
+    button = Button(windows, text="Responder", bg="gray15", fg="white", relief="flat", cursor="hand2", bd=0, padx=10,
+                    command=chat_gpt)
     button.config(width=10, height=2, borderwidth=0, highlightthickness=0, highlightbackground="gray",
-                  highlightcolor="gray", bd=0, pady=0, padx=10, takefocus=0, )
+                  highlightcolor="gray", bd=0, pady=0, padx=10, takefocus=0)
     button.pack(pady=5)
+
     windows.mainloop()
